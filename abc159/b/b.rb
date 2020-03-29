a8b36f9bd482e0aeae8025568b6f740e81aafdbd
Shota Yamazaki <sysy5867@gmail.com>
2020-03-29 21:23:52 +0900

    add b.rb

--- abc159/b/b.rb
+++ abc159/b/b.rb
@@ -1,18 +1,12 @@
-str = gets.chop.split('')
-str_count = str.count
+input = gets.chop.split('')
 
-def palindrome?(array)
-  count = array.count
-  mid = count / 2
-  array[0..mid - 1] == array[mid + 1..-1].reverse
+def palindrome?(str)
+  str == str.reverse
 end
 
+answer = true
 
-if str_count == 3
-  return puts palindrome?(str) ? 'Yes' : 'No'
-else
-  a = palindrome?(str)
-  b = palindrome?(str[0..((str_count - 1 / 2) - 1)])
-  c = palindrome?([((str_count + 3) / 2) - 1..-1])
-  return puts a && b && c ? 'Yes' : 'No'
-end
+answer = false if !palindrome?(input)
+answer = false if !palindrome?(input[0..(input.count - 1) / 2 - 1])
+
+puts answer ? 'Yes' : 'No'
