542dac7c95f14d05a9ad67bb64d4062858c291df
Shota Yamazaki <sysy5867@gmail.com>
2020-04-09 11:31:19 +0900

    add

---
+++ abc155/d/d.rb
@@ -0,0 +1,11 @@
+N, K = gets.chop.split.map(&:to_i)
+A = gets.chop.split(' ').map(&:to_i)
+
+# array = []
+# (0..(N - 1)).each do |i|
+#   ((i + 1)..(N - 1)).each do |j|
+#     array.push(A[i] * A[j])
+#   end
+# end
+
+# puts array.sort[K - 1]
