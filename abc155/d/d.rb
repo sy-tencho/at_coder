4e57cff49d388356586b2c23e61afd82d55738dd
Shota Yamazaki <sysy5867@gmail.com>
2020-04-11 21:54:16 +0900

    add

--- abc155/d/d.rb
+++ abc155/d/d.rb
@@ -1,59 +1,70 @@
 N, K = gets.chop.split.map(&:to_i)
 A = gets.chop.split(' ').map(&:to_i)
 
-# array = []
-# (0..(N - 1)).each do |i|
-#   ((i + 1)..(N - 1)).each do |j|
-#     array.push(A[i] * A[j])
-#   end
-# end
-
-# puts array.sort[K - 1]
-
-neg = []
-zer = []
-pos = []
+neg_arr = []
+zer_arr = []
+pos_arr = []
 
 A.each do |i|
   if i == 0
-    zer.push(i)
+    zer_arr.push(i)
   elsif i > 0
-    pos.push(i)
+    pos_arr.push(i)
   else
-    neg.push(i)
+    neg_arr.push(i)
   end
 end
 
-neg_c = neg.count
-pos_c = pos.count
-zer_c = zer.count
+neg_count = neg_arr.count
+zer_count = zer_arr.count
+pos_count = pos_arr.count
 
-if K <= neg_c * pos_c
-  ans = []
-  neg.each do |i|
-    pos.each do |j|
-      ans.push(i * j)
+neg_ans_count = neg_count * pos_count
+zer_ans_count = zer_count * (neg_count + pos_count) + (zer_count * (zer_count - 1) / 2)
+pos_ans_count = (neg_count * (neg_count - 1) / 2) + (pos_count * (pos_count - 1) / 2)
+
+total_count = neg_ans_count + zer_ans_count + pos_ans_count
+# test
+# puts total_count == (N * (N - 1) / 2)
+
+def comb(arr)
+  tmp = []
+  if arr.count == 3
+    tmp.push(arr[0] * arr[1])
+    return tmp
+  else
+    (0..(arr.count - 2)).each do |i|
+      ((i+1)..(arr.count - 1)).each do |j|
+        tmp.push(arr[i] * arr[j])
+      end
+    end
+    return tmp
+  end
+end
+
+
+if K <= neg_ans_count
+  neg_ans_arr = []
+  neg_arr.each do |n|
+    pos_arr.each do |p|
+      neg_ans_arr.push(n * p)
     end
   end
-  puts ans[K - 1]
-elsif K > neg_c * pos_c && K <= neg_c * pos_c + (zer_c * (zer_c - 1)) / 2 + zer_c * (neg_c + pos_c)
+  puts neg_ans_arr[-1]
+elsif K > neg_ans_count && K < neg_ans_count + zer_ans_count
   puts 0
 else
-  ans = []
-  (0..neg_c-2).each do |i|
-    (i+1..neg_c-1).each do |j|
-      ans.push(neg[i] * neg[j])
-    end
+  pos_ans_arr = []
+  
+  unless pos_arr.count < 2
+    pos_ans_arr.concat(comb(pos_arr))
   end
 
-  (0..pos_c-2).each do |i|
-    (i+1..pos_c-1).each do |j|
-      ans.push(pos[i] * pos[j])
-    end
+  unless neg_arr.count < 2
+    pos_ans_arr.concat(comb(neg_arr))
   end
 
-  ans.sort!
-
-  l = K - neg_c * pos_c + (zer_c * (zer_c - 1)) / 2
-  puts ans[l -1]
+  k = K - neg_ans_count - zer_ans_count - 1
+  pos_ans_arr.sort!
+  puts pos_ans_arr[k]
 end
