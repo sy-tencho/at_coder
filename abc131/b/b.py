73eee2772a541e9189fc5099391515b2d68c3353
Shota Yamazaki <sysy5867@gmail.com>
2020-04-21 14:52:09 +0900

    abc131 b

--- abc131/b/b.py
+++ abc131/b/b.py
@@ -1,3 +1,15 @@
 #!/usr/bin/env python3
 n, l = map(int, input().split())
 
+li = list(i for i in range(1, n+1, 1))
+ab = 10000
+ans = 0
+
+for i in li:
+  if abs(l + i - 1) < ab:
+    ab = abs(l + i - 1)
+    ans = l + i - 1
+
+su = sum(li) + n * l - n
+
+print(su - ans)
